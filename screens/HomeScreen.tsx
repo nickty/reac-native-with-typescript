e5b14b9112e06faad25731538016eca76ac37c16
Mizanur Rahman <nickty.86@gmail.com>
2022-10-26 14:43:58 +0600

    data.json

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,18 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
+
+import data from "../data.json";
 
 const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
   return (
     <View>
       <Text>HomeScreen</Text>
+      <Text>{JSON.stringify(data, null, 4)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet({});
 
 export default HomeScreen;
