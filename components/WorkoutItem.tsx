d873a0eb823d0cd5c1b99834327ba7b07ae12a52
Mizanur Rahman <nickty.86@gmail.com>
2022-10-26 15:43:10 +0600

    stylings

---
+++ components/WorkoutItem.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Workout } from "../types/data";
+
+const WorkoutItem = ({ item }: Workout) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{item.name}</Text>
+      <Text style={styles.duration}>{item.duration}</Text>
+      <Text style={styles.difficulty}>{item.difficulty}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: 10,
+    borderColor: "rgba(0,0,0,0.1)",
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 15,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  duration: {
+    fontSize: 15,
+  },
+  difficulty: {
+    fontSize: 15,
+  },
+});
+
+export default WorkoutItem;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,21 +4,15 @@ import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 
 import data from "../data.json";
 import { Workout } from "../types/data";
+import WorkoutItem from "../components/WorkoutItem";
 
 const HomeScreen = ({ navigation }: NativeStackHeaderProps) => {
-  const renderItem = ({ item }: { item: Workout }) => (
-    <View>
-      <Text>{item.name}</Text>
-      <Text>{item.difficulty}</Text>
-    </View>
-  );
-
   return (
     <View style={styles.container}>
       {/* <Text>HomeScreen</Text> */}
       <FlatList
         data={data as Workout[]}
-        renderItem={renderItem}
+        renderItem={({ item }) => <WorkoutItem item={item} />}
         keyExtractor={(item) => item.slug}
       />
     </View>
