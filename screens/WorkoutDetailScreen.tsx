4beed4faeb8a149c78a29d81d3bd76f18d2071bb
Mizanur Rahman <nickty.86@gmail.com>
2022-10-27 13:53:55 +0600

    Navigation type

--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -2,10 +2,20 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack/lib/typescript/src/types";
 
-const WorkoutDetailScreen = ({ navigation, route }: NativeStackHeaderProps) => {
+type DetailParams = {
+  route: {
+    params: {
+      slug: string;
+    };
+  };
+};
+
+type Navigation = NativeStackHeaderProps & DetailParams;
+
+const WorkoutDetailScreen = ({ navigation, route }: Navigation) => {
   return (
     <View>
-      <Text> Slug - {(route.params as any).slug}</Text>
+      <Text> Slug - {route.params.slug}</Text>
     </View>
   );
 };
